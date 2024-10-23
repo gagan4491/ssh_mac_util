901d96ae379af77dfaa2ef097404d37ce8c89f15
gagan <gsingh@alertdriving.com>
2024-10-23 18:28:57 -0400

    initial commit

---
+++ mac_util.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import subprocess
+import argparse
+
+def open_ssh_session(host):
+    apple_script = f'''
+    tell application "Terminal"
+        do script "ssh {host}"
+        activate
+    end tell
+    '''
+    # Use subprocess to execute the AppleScript
+    subprocess.run(["osascript", "-e", apple_script])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Open an SSH session.")
+    parser.add_argument('selected_text', nargs='?', default=None, help='The selected text to open SSH session for')
+    args = parser.parse_args()
+
+    if args.selected_text:
+        # Get the IP or hostname before the slash if needed
+        final_host = str(args.selected_text.split('/')[0]).strip()
+        open_ssh_session(final_host)
+    else:
+        print("Please provide the selected text to open an SSH session.")
